5bcce66867b885090dbe21f07b553ed53d03d9e1
urvashi29 <urvashigolu29@gmail.com>
2026-04-07 00:02:53 +0530

    added session-1

---
+++ session-1/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+// Function based component
+function App() {
+  return (
+    // JSX
+    <>
+      <Header />
+      <p>Hello</p>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+// render means to display
