0dfc060c9007e947a18c958725e945257fd3858f
urvashi29 <urvashigolu29@gmail.com>
2026-05-07 00:04:05 +0530

    added routing params

--- session-4/code/src/pages/ProductDetails.jsx
+++ session-4/code/src/pages/ProductDetails.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const ProductDetails = () => {
-    return (
-        <div>
-            ProductDetails
-        </div>
-    );
+  const [product, setProduct] = useState(null);
+  const productId = useParams();
+  console.log(productId.pid);
+
+  const fetchProduct = async () => {
+    try {
+      let response = await axios.get(
+        `https://dummyjson.com/products/${productId.pid}`,
+      );
+      console.log(response.data);
+      setProduct(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  return <div>{product && product?.brand}</div>;
 };
 
 export default ProductDetails;
